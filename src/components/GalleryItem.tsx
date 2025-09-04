4b6dcc4c720cbc55c5bb017fe7d43557ddd2e2b9
Tiffany <yuichitlo@users.noreply.github.com>
2025-09-04 12:26:32 -0400

    Optimize image loading using Astro Image

--- src/components/GalleryItem.tsx
+++ src/components/GalleryItem.tsx
@@ -2,13 +2,12 @@
 import { useState } from "react";
 import { setLetter } from "../store";
 
-export default function GalleryItem({letter}) {
+export default function GalleryItem({letter, children}) {
   return letter && (
     <div className="gallery-item" onClick={() => {
-      alert(`You clicked ${letter.data.letter}`);
       setLetter({...letter.data, content: letter.body});
     }}>
-      {letter.data?.dropcap && <img src={letter.data.dropcap.src} alt=""/>}
+      {children}
     </div>
   );
 }
